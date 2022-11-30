ec74cda45d87c025e066211558628860c66d390c
todd <andersontodds@gmail.com>
2022-11-29 18:04:04 -0800

    let mlat converter take mix of vector and scalar input

--- geog2geom.m
+++ geog2geom.m
@@ -1,5 +1,6 @@
 function geom_lat = geog2geom(geog_lat, geog_long, height, decimalYear)
-% From Daniel Okoh on MATLAB File Exchange
+% Original function from Daniel Okoh on MATLAB File Exchange;
+% adapted by Todd Anderson November 2022
 % https://www.mathworks.com/matlabcentral/fileexchange/102219-convert-from-geographic-to-geomagnetic-coordinates?s_tid=prof_contriblnk
 %   
 %INPUTS:
@@ -12,7 +13,30 @@ function geom_lat = geog2geom(geog_lat, geog_long, height, decimalYear)
 %geom_lat: Geomagnetic latitude, specified as a scalar or vector, in degrees. North latitude is positive and south latitude is negative.
 %The size of geom_lat is same as the size of each of the inputs
 %number of data points
-n=length(geog_lat);
+
+n1=length(geog_lat);
+n2=length(geog_long);
+n3=length(height);
+n4=length(decimalYear);
+
+n = max([n1 n2 n3 n4]);
+
+if length(geog_lat) < n
+    geog_lat = geog_lat.*ones(n, 1);
+end
+
+if length(geog_long) < n
+    geog_long = geog_long.*ones(size(geog_lat));
+end
+
+if length(height) < n
+    height = height.*ones(size(geog_lat));
+end
+
+if length(decimalYear) < n
+    decimalYear = decimalYear.*ones(size(geog_lat));
+end
+
 geom_lat=nan(size(geog_lat));
 for i=1:n
     %First compute the magnetic dip I(also known as the magnetic inclination or dip angle)
